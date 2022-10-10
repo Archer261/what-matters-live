4f6d3ea92c238161e9179430e948232a7f8da933
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-09 22:28:27 -0400

    working graphql api

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,9 @@ import Head from 'next/head';
 // import Image from 'next/image'
 // import { title } from 'process';
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'She-Hulk: Emasculation at Large', excerpt: 'Everything Wrong With Episode 7' },
-  { title: 'She-Hulk: Emasculation at Large2', excerpt: 'Everything Wrong With Episode 7' },
-  { title: 'She-Hulk: Emasculation at Large3', excerpt: 'Everything Wrong With Episode 7' }
-];
-
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -30,8 +24,13 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts }
+  }
 }
